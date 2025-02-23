a96f7e515cdfd91b5d29fe1bd5c951d461d94ecf
CsikSzabi04 <68150404+CsikSzabi04@users.noreply.github.com>
2025-02-23 12:18:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 
 const PORT = 88;
 const API_URL = "https://api.rawg.io/api/games";
-const API_KEY = "984255fceb114b05b5e746dc24a8520a";
+const API_KEY = "984255fceb114b05b5e746dc24a8520a"; //https://rawg.io/@csszabj04/apikey
 const DATA_FILE = "games.json";
 
 async function fetchGames(req, res) {
@@ -52,12 +52,52 @@ async function getFreeGames(req, res) {
     }
 }
 
+async function getNews(req, res) {
+    try {
+        const response = await fetch("https://api.steampowered.com/ISteamNews/GetNewsForApp/v0002/?appid=440&count=350&maxlength=300&format=json");
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
 
+async function getDiscounted(req, res) {
+    try {
+        const response = await fetch("https://store-site-backend-static.ak.epicgames.com/freeGamesPromotions");
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+}
 
+async function getFree(req, res) {
+    const url = 'https://free-to-play-games-database.p.rapidapi.com/api/games';
+    const options = {
+        method: 'GET',
+        headers: {
+            'x-rapidapi-key': 'b05744bab0mshe91c13f2d427740p11f35djsnacb7b089052e',
+            'x-rapidapi-host': 'free-to-play-games-database.p.rapidapi.com'
+        }
+    };
+
+    try {
+        const response = await fetch(url, options);
+        const data = await response.json();
+        res.json(data);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+app.get("/", (req, res) => res.send("<h1>It's all good :)</h1>"));
 app.get("/fetch-games", fetchGames);
 app.get("/stores", getSores);
 app.get("/game", getGames);
-app.get("/freegames", getFreeGames);
+app.get("/news", getNews);
+app.get("/free", getFree);
+app.get("/discounted", getDiscounted);
 
 app.listen(PORT, () => {
     console.log(`Server running on port :${PORT}`);
